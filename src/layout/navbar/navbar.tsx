8b4031783557a412fc470b28e8a9d0e3ad08a65f
Roman Litvinov <litvinovroman911@gmail.com>
2022-09-26 22:00:59 +0100

    Implemented navigation bar.

---
+++ src/layout/navbar/navbar.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+import * as styles from './navbar.module.scss'
+import { NavLink } from 'react-router-dom'
+
+export const AppNavbar: FC = () => {
+    return (
+        <nav className="">
+            <ul className={styles.links}>
+                <li>
+                    {' '}
+                    <NavLink to="/diseases">Diseases</NavLink>
+                </li>
+                <li>
+                    {' '}
+                    <NavLink to="/symptoms">Symptoms</NavLink>
+                </li>
+                <li>
+                    {' '}
+                    <NavLink to="/risks">Risks</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
