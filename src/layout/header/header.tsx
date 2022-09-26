d01e69e1c7d83c06b31a5ed91f6e16d60dca23ef
Roman Litvinov <litvinovroman911@gmail.com>
2022-09-26 22:05:03 +0100

    Feature/Navigation Bar (#8)
    * Implemented navigation bar.
    
    * Code review fixes

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -3,6 +3,8 @@ import { AuthService } from '../../auth/auth.service'
 import * as styles from './header.module.scss'
 import { Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import HealingIcon from '@mui/icons-material/Healing'
+import { AppNavbar } from '../navbar/navbar'
 
 export const AppHeader: FC = () => {
     const [username, setUserName] = useState('John Doe')
@@ -18,7 +20,12 @@ export const AppHeader: FC = () => {
     return (
         <header className={styles.header}>
             <div className={styles.headerContent}>
-                <h1 className={styles.headerTitle}>Medical Admin Portal</h1>
+                <div className={styles.logo}>
+                    <HealingIcon fontSize="large" />
+                    <h1 className={styles.headerTitle}>Admin Portal</h1>
+                </div>
+                <AppNavbar />
+
                 <span>{username}</span>
                 <Button type="button" onClick={signOut}>
                     Sign out
